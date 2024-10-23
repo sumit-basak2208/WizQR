098aed706c8ddfc0181a56b2b4178122a81770d9
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-23 21:36:02 +0530

    changed UI

--- app/template.tsx
+++ app/template.tsx
@@ -16,7 +16,7 @@ export default function DashboardTemplate({
       {/* <div className="bg-stone-200/90 grid grid-cols-[220px,_1fr] h-[calc(100dvh-52px)] gap-4 p-4"> */}
       <div className="bg-stone-200/90 h-[calc(100dvh-52px)] gap-4 p-4">
         {/* <SideNavbar/> */}
-        <div className="bg-white rounded-lg p-3 pb-4 shadow h-[calc(100dvh-90px)]">
+        <div className="bg-white rounded-lg p-3 pb-4 shadow min-h-[calc(100dvh-90px)]">
           <div className="max-w-6xl mx-auto">{children}</div>
         </div>
       </div>
