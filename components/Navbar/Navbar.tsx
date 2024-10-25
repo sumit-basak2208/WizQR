5f18b17237d74c721b539a7fcb25aa74419325e0
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-25 20:55:42 +0530

    created api folder structure

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import Account from "./Account";
 
 export default function Navbar() {
     return (
-      <nav className="flex justify-between items-center sticky bg-white px-4 py-2 shadow">
+      <nav className="flex justify-between items-center sticky top-0 bg-white px-4 py-2 shadow">
         <h1 style={{ fontFamily: "cursive" }} className="text-lg font-bold">WizQR</h1>
         <Account isLoggedIn={true} />
       </nav>
