3b7398d2d400bfce45298614ee9938de24deab0a
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-26 20:27:07 +0530

    creadet register route

---
+++ app/api/v1/user/register/route.ts
@@ -0,0 +1,37 @@
+import connnect from "@/db/db";
+import Users from "@/models/user.model";
+import { generateHash } from "@/utils/encryption";
+import { NextRequest, NextResponse } from "next/server";
+
+connnect();
+
+export async function POST(req: NextRequest) {
+  try {
+    const {
+      username,
+      email,
+      password,
+    }: { username: string; email: string; password: string } = await req.json();
+
+    const newUser = new Users({
+      username,
+      email,
+      password: generateHash(password),
+    });
+
+    await newUser.save();
+    return NextResponse.json({
+      message: "Registration successfull",
+    });
+  } catch (err: any) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: err?.message || "Registration failed, please try again later!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
