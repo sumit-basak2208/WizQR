f6fafd0c6356263b07fea95d8188ddbb208b71a1
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-26 20:26:49 +0530

    created login route

---
+++ app/api/v1/user/login/route.ts
@@ -0,0 +1,59 @@
+import connnect from "@/db/db";
+import Users from "@/models/user.model";
+import { checkPassword } from "@/utils/encryption";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+connnect();
+
+export async function POST(req: NextRequest) {
+  try {
+    const { username, password } = await req.json();
+    const user = await Users.findOne({
+      $or: [{ username: username }, { email: username }],
+    });
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "User not found, please try again later!",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    if (!checkPassword(password, user.password)) {
+      return NextResponse.json(
+        {
+          error: "Wrong password!!",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+    cookies().set(
+      "token",
+      jwt.sign(
+        {
+          userId: user._id,
+        },
+        process.env.JWT_TOKEN ?? ""
+      )
+    );
+    return NextResponse.json({
+      message: "Login successfull!",
+    });
+  } catch (err: any) {
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: err?.message || "Login failed, please try again later!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
