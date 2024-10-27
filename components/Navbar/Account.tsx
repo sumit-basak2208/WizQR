9ef9e359c3c0a84da2ecae9d07a0798d2ddbc025
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-27 21:22:57 +0530

    added login link

--- components/Navbar/Account.tsx
+++ components/Navbar/Account.tsx
@@ -1,43 +1,61 @@
-import { User } from "lucide-react";
+"use client";
+import { LogIn, User } from "lucide-react";
 import Link from "next/link";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { deleteCookie, hasCookie } from "cookies-next";
+import { useMemo } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-export default function Account({ isLoggedIn }: { isLoggedIn: boolean }) {
+export default function Account() {
+  const router = useRouter();
+
+  const isLoggedIn = useMemo(() => hasCookie("token"), []);
+
+  const logout = () => {
+    deleteCookie("token");
+    toast.success("Logout successfull!");
+    router.push("/");
+  };
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <button
-          type="button"
-          className="relative flex rounded-full p-1 border border-double text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          id="user-menu-button"
-          aria-expanded="false"
-          aria-haspopup="true"
+    <>
+      {!isLoggedIn ? (
+        <Link
+          href="/login"
+          // onClick={() => signIn("google", { callbackUrl: "/chat" })}
+          className="flex items-center font-semibold gap-2"
         >
-          <span className="absolute -inset-1.5"></span>
-          <span className="sr-only">Open user menu</span>
-          <User />
-        </button>
-      </PopoverTrigger>
-      <PopoverContent className="p-1 w-40">
-        <a
-          href="#"
-          className="block px-4 py-1 text-sm"
-        >
-          Your Profile
-        </a>
-        <a
-          href="#"
-          className="block px-4 py-1 text-sm"
-        >
-          Dashboard
-        </a>
-        <a
-          href="#"
-          className="block px-4 py-1 text-sm"
-        >
-          Sign out
-        </a>
-      </PopoverContent>
-    </Popover>
+          <span className="mb-1">Login</span>
+          <LogIn size={20} />
+        </Link>
+      ) : (
+        <Popover>
+          <PopoverTrigger asChild>
+            <button
+              type="button"
+              className="relative flex rounded-full p-1 border border-double text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              id="user-menu-button"
+              aria-expanded="false"
+              aria-haspopup="true"
+            >
+              <span className="absolute -inset-1.5"></span>
+              <span className="sr-only">Open user menu</span>
+              <User />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent className="p-1 w-40">
+            <Link href="/profile" className="block px-4 py-1 text-sm">
+              Your Profile
+            </Link>
+            <Link href="/dashboard" className="block px-4 py-1 text-sm">
+              Dashboard
+            </Link>
+            <button onClick={() => logout()} className="block px-4 py-1 text-sm">
+              Sign out
+            </button>
+          </PopoverContent>
+        </Popover>
+      )}
+    </>
   );
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
-import Account from "./Account";
+import dynamic from "next/dynamic";
+
+const Account = dynamic(() => import("./Account"), {
+  loading: () => <></>,
+  ssr: false
+});
 
 export default function Navbar() {
     return (
-      <nav className="flex justify-between items-center sticky top-0 bg-white px-4 py-2 shadow">
+      <nav className="flex justify-between items-center sticky top-0 bg-white px-4 py-2 border-b-2 border-black">
         <h1 style={{ fontFamily: "cursive" }} className="text-lg font-bold">WizQR</h1>
-        <Account isLoggedIn={true} />
+        <Account />
       </nav>
     );
 }
