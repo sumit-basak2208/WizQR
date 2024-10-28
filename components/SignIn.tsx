36ff8ae7acd8ead7c24bc202261a0339a34a18bc
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-28 20:31:49 +0530

    added validation

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -3,13 +3,29 @@ import { LogInIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function SignIn() {
-  const [isLogIn, setIsLogIn] = useState(true);
+    const [isLogIn, setIsLogIn] = useState(true);
+
+  const login = async (ev: React.FormEvent) => {
+    ev.preventDefault();
+    try {
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+  const register = async (ev: React.FormEvent) => {
+    ev.preventDefault();
+    try {
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <h1 className="text-4xl font-bold mb-3">
         {isLogIn ? "Login" : "Register"}
       </h1>
-      <form>
+      <form onSubmit={(ev) => (isLogIn ? login(ev) : register(ev))}>
         {isLogIn ? (
           <>
             <div className="mt-2">
@@ -17,16 +33,20 @@ export default function SignIn() {
                 Username or email
               </label>
               <input
+                name="username"
                 className="max-w-full w-80 block p-2 rounded-sm border"
                 placeholder="Username or email"
+                required
               />
             </div>
             <div className="mt-2">
               <label className="text-gray-600 text-xs ml-1">Password</label>
               <input
+                name="password"
                 type="password"
                 className="max-w-full w-80 block p-2 rounded-sm border"
                 placeholder="password"
+                required
               />
             </div>
           </>
@@ -35,24 +55,30 @@ export default function SignIn() {
             <div className="mt-2">
               <label className="text-gray-600 text-xs ml-1">Username</label>
               <input
+                name="username"
                 className="max-w-full w-80 block p-2 rounded-sm border"
                 placeholder="Username"
+                required
               />
             </div>
             <div className="mt-2">
               <label className="text-gray-600 text-xs ml-1">Email</label>
               <input
+                name="email"
                 type="email"
                 className="max-w-full w-80 block p-2 rounded-sm border"
                 placeholder="Email"
+                required
               />
             </div>
             <div className="mt-2">
               <label className="text-gray-600 text-xs ml-1">Password</label>
               <input
+                name="password"
                 type="password"
                 className="max-w-full w-80 block p-2 rounded-sm border"
                 placeholder="password"
+                required
               />
             </div>
           </>
@@ -61,7 +87,7 @@ export default function SignIn() {
           <p className="-translate-x-2">{isLogIn ? "Login" : "Register"}</p>
 
           <div className="bg-purple-400 rounded-lg h-12 w-1/4 flex items-center text-white justify-center absolute right-0 top-0 group-hover:w-80 z-10 duration-500">
-            <LogInIcon strokeWidth={2.5}/>
+            <LogInIcon strokeWidth={2.5} />
           </div>
         </button>
       </form>
