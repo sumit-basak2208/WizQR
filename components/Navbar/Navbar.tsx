1ba94efe4d7e22001611953b3972defb21d55117
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-29 22:34:13 +0530

    change navbar UI

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ const Account = dynamic(() => import("./Account"), {
 
 export default function Navbar() {
     return (
-      <nav className="flex justify-between items-center sticky top-0 bg-white border-b-2 border-black">
+      <nav className="flex justify-between items-center sticky top-0 bg-white border-b-2 border-black z-50">
         <Link
           href="/"
           style={{ fontFamily: "cursive" }}
