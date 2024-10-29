af9e928d6c9519201bfaaeb6f1f2a08699bf6c1b
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-10-29 22:33:14 +0530

    changed text

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,14 @@ export default function Home() {
       <main className="grid md:grid-cols-2 grid-cols-1 overflow-hidden">
         <div className="flex border-r-4 border-black relative items-center">
           <div className="ml-10 mb-10">
-            <h1 className="text-5xl font-bold">Lorem Ipsum</h1>
-            <p className="text-lg text-gray-800 mt-3 max-w-[500px]">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
+            <h1 className="text-3xl font-bold">
+              Create Your Custom QR-Linked <br/>Cards with WizQR
+            </h1>
+            <p className="text-gray-800 mt-3 max-w-[500px]">
+              Design unique, eye-catching cards that seamlessly connect your
+              audience to your website with a simple QR scan. Customize every
+              detail, add your personalized link, and watch your brand come to
+              life on a card that’s just as unique as you are.
             </p>
             <div className="w-fit bg-black rounded">
               <button className="flex gap-4 px-4 py-2 bg-white text-xl transition-all translate-x-1 -translate-y-1 active:!translate-x-0 active:!translate-y-0 font-bold border-2 border-black rounded mt-5">
