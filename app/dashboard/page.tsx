132ed9ec7c2163d4073a5167ae6365725bc29bcc
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-02 19:01:30 +0530

    added grid

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,20 +4,22 @@ export default function Home() {
   return (
     <main className="relative">
       <nav className="-mx-3 -mt-2 px-3 py-2 flex justify-between items-center sticky top-0 bg-white border-b-2 border-black z-50">
-        <h2
-          style={{ fontFamily: "cursive" }}
-          className="text-lg font-bold"
-        >
+        <h2 style={{ fontFamily: "cursive" }} className="text-lg font-bold">
           WizQRs
         </h2>
         <div className="w-fit h-fit bg-black rounded">
-          <button title="Add card" className="flex items-center gap-2 px-1 py-1 bg-white transition-all translate-x-0.5 -translate-y-0.5 hover:-translate-y-1 hover:translate-x-1 active:!translate-x-0 active:!translate-y-0 font-bold border-2 border-black rounded">
-            <BadgePlus strokeWidth={2.5} size={18} className="mt-0.5"/>
+          <button
+            title="Add card"
+            className="flex items-center gap-2 px-1 py-1 bg-white transition-all translate-x-0.5 -translate-y-0.5 hover:-translate-y-1 hover:translate-x-1 active:!translate-x-0 active:!translate-y-0 font-bold border-2 border-black rounded"
+          >
+            <BadgePlus strokeWidth={2.5} size={18} className="mt-0.5" />
             <span>Card</span>
           </button>
         </div>
       </nav>
-      <div className="grid "></div>
+      <div className="grid grid-cols-5 gap-6">
+        <div className="w-full"></div>
+      </div>
     </main>
   );
 }
