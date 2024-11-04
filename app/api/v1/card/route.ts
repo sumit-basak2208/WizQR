a0156ea08ca3e690c52dea846646cc6fcd137145
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-04 18:39:43 +0530

    added card route

---
+++ app/api/v1/card/route.ts
@@ -0,0 +1,46 @@
+import Cards from "@/models/card.model";
+import { useSearchParams } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const page = searchParams.get("page");
+    console.log(page);
+    return NextResponse.json(
+      {
+        error: "Something went wrong, please try again later!",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: err?.message || "Something went wrong, please try again later!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: err?.message || "Something went wrong, please try again later!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
