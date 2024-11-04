715b8d00e704185dba7b57bc936b5c7007011bdd
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-04 18:38:49 +0530

    added create card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { BadgePlus } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="relative">
       <nav className="-mx-3 -mt-2 px-3 py-2 flex justify-between items-center sticky top-0 bg-white border-b-2 border-black z-50">
@@ -8,13 +9,14 @@ export default function Home() {
           WizQRs
         </h2>
         <div className="w-fit h-fit bg-black rounded">
-          <button
+          <Link
+            href="/dashboard/card"
             title="Add card"
             className="flex items-center gap-2 px-1 py-1 bg-white transition-all translate-x-0.5 -translate-y-0.5 hover:-translate-y-1 hover:translate-x-1 active:!translate-x-0 active:!translate-y-0 font-bold border-2 border-black rounded"
           >
             <BadgePlus strokeWidth={2.5} size={18} className="mt-0.5" />
             <span>Card</span>
-          </button>
+          </Link>
         </div>
       </nav>
       <div className="grid grid-cols-5 gap-6 my-5">
