94df7b54cd3b9e0ef08f2f198bcc231946999782
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-04 19:19:58 +0530

    added path in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,34 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { getTokenData } from "./utils/token";
 
 export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/dashboard")) {
+  if (
+    ["/dashboard", "/api/v1/user", "/api/v1/card"].some((ele) =>
+      req.nextUrl.pathname.startsWith(ele)
+    )
+  ) {
     if (cookies().has("token")) return NextResponse.next();
     const url = req.nextUrl.clone();
     url.pathname = "/";
     url.searchParams.set("login", "true");
     return NextResponse.redirect(url);
   }
+
+  // if (
+  //   ["/api/v1/user", "/api/v1/card"].some((ele) =>
+  //     req.nextUrl.pathname.startsWith(ele)
+  //   )
+  // ) {
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = "/";
+  //   url.searchParams.set("login", "true");
+  //   if (!cookies().has("token")) {
+  //     return NextResponse.redirect(url);
+  //   }
+  // }
 }
 
 export const config = {
-  matcher: ["/dashboard/:path", "/api/:path"],
+  matcher: ["/dashboard/:path", "/api/v1/card/:path", "/api/v1/user"],
 };
