92856259d6adbc63acbdb26a792b19878af79662
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-05 20:54:16 +0530

    added timestamp

--- models/card.model.ts
+++ models/card.model.ts
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
-const cardSchema = new mongoose.Schema({
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
+const cardSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    url: String,
+    gradientType: {
+      type: String,
+      enum: ["linear", "radial"],
+    },
+    position: [Number],
+    backgroundColors: [String],
+    QRColors: [String],
+    points: [Number],
+    urlCode: {
+      type: String,
+      unique: true,
+    },
   },
-  url: String,
-  gradientType: {
-    type: String,
-    enum: ["linear", "radial"],
-  },
-  position: [Number],
-  backgroundColors: [String],
-  QRColors: [String],
-  points: [Number],
-  urlCode: {
-    type: String,
-    unique: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Cards = mongoose.models.cards || mongoose.model("cards", cardSchema);
 export default Cards;
--- models/user.model.ts
+++ models/user.model.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    email: {
+      type: String,
+      unique: true,
+    },
   },
-  password: String,
-  email: {
-    type: String,
-    unique: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Users = mongoose.models.users || mongoose.model("users", userSchema);
 export default Users;
