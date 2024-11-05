0480801a891a7fd0c2a2d99b3fb3af65396f39dd
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-05 20:30:54 +0530

    fetched card list

--- app/api/v1/card/route.ts
+++ app/api/v1/card/route.ts
@@ -1,21 +1,48 @@
 import Cards from "@/models/card.model";
+import { getTokenData } from "@/utils/token";
+import { cookies } from "next/headers";
 import { useSearchParams } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
+    // Check if user is authorized
+    const tokenCookie = cookies().get("token");
+    if (!tokenCookie)
+      return NextResponse.json(
+        {
+          error: "Unauthorized!",
+        },
+        {
+          status: 401,
+        }
+      );
+    const dt = getTokenData(tokenCookie.value);
+    const data = dt as { userId: string } | boolean;
+    if (!data || typeof data != "object")
+      return NextResponse.json(
+        {
+          error: "Unauthorized!",
+        },
+        {
+          status: 401,
+        }
+      );
+    // Get pagenumber
     const searchParams = req.nextUrl.searchParams;
     let page: number = Number(searchParams.get("page"));
-      if (!page) page = 1;
-    
-    return NextResponse.json(
-      {
-        error: "Something went wrong, please try again later!",
-      },
-      {
-        status: 200,
-      }
-    );
+    if (!page) page = 1;
+
+    // fetch cards
+    const cards = await Cards.find({ owner: data.userId })
+      .skip(page - 1 * 10)
+      .limit(10);
+
+    return NextResponse.json({
+      message: "Cards fetched ssuccessfully",
+      cards,
+      status: 200,
+    });
   } catch (error: unknown) {
     const err = error as Error;
     console.log(err);
