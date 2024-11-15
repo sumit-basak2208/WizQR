1a1b04fd70f3b5a46488ee08e6f8a9358eb811a2
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-11-15 20:09:13 +0530

    created redirect API

---
+++ app/api/v1/redirect/[id]/route.ts
@@ -0,0 +1,45 @@
+import connnect from "@/db/db";
+import Cards from "@/models/card.model";
+import { getUserId } from "@/utils/utility";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+connnect();
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const data = await params;
+    const card = await Cards.findOneAndUpdate(
+      { urlCode: data.id },
+      { $inc: { totalScans: 1 } }
+    );
+    if (!card) {
+      return NextResponse.json(
+        {
+          error: "Card not found!",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    return NextResponse.json({
+      redirectUrl: card.url,
+      status: 200,
+    });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(err);
+    return NextResponse.json(
+      {
+        error: err?.message || "Something went wrong, please try again later!",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
