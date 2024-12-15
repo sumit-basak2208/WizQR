fa1a8f1e86cb5cc09b919ca14c6f6595001f7100
sumit-basak2208 <sumitbasak2208@gmail.com>
2024-12-15 18:52:19 +0530

    added reset-password API

---
+++ app/api/v1/user/reset-password/page.tsx
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/models/user.model";
+import connectDB from "@/db/db";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const { token, newPassword } = await req.json();
+
+    // Find the user with the matching token and ensure it is not expired
+    const user = await User.findOne({
+      passwordResetToken: token, // Use raw token directly
+      passwordResetExpires: { $gt: Date.now() }, // Token expiration check
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Invalid or expired token" },
+        { status: 400 }
+      );
+    }
+
+    user.password = newPassword;
+
+    // Clear the password reset fields
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+    await user.save(); // Save the updated user to the database
+
+    return NextResponse.json(
+      { message: "Password updated successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error in Reset Password API:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
